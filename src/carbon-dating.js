3edab77b1aee8ec420024e82d4ce33783ab5df24
Kirill Scherbakoff <elkirrs@gmail.com>
2020-03-14 15:42:03 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,13 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if("string" != typeof(sampleActivity) || isNaN(parseFloat(sampleActivity))) {
+    return false;
+  }
+
+  let res = Math.ceil(HALF_LIFE_PERIOD * Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / 0.693);
+  return (res > 0 && res !== Infinity)
+      ? res
+      : false;
 };
