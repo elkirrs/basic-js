f993851612829f945ca2f54791ae81b5322dac0a
Kirill Scherbakoff <elkirrs@gmail.com>
2020-03-22 23:31:19 +0300

    feat calculate Hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,6 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-}
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let n = {};
+    n.turns = Math.pow(2, disksNumber) - 1;
+    n.seconds = n.turns / (turnsSpeed / 3600);
+    return n;
+};
