f4078cfcea3ae710530f9c2c7eaf0e5e4a427ff4
Kirill Scherbakoff <elkirrs@gmail.com>
2020-03-22 23:04:34 +0300

    feat: what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,14 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if (date.hasOwnProperty('getMonth')) {
+    throw Error;
+  }
+
+  let seasons = [
+    'winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'fall', 'fall', 'fall', 'winter'
+  ];
+
+  return seasons[date.getMonth()];
 };
